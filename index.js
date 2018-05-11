2e93588f9d1e7e9866fb1a74d0047582a3a9a722
Omer <omerfu@gmail.com>
2018-05-10 17:34:05 -0700

    done!

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express=require('express');
 const cors=require('cors');
+const {resolve} = require('path');
 const PORT=process.env.PORT || 9000;
 
 const app=express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
 
-app.get('/user-data', (req, res)=>{
+app.post('/api/send-data', (req, res)=>{
+    console.log('Data Sent:', req.body);
+
+    res.send({success:true, mirror:req.body});
+});
+
+app.get('/api/user-data', (req, res)=>{
     const user={
         name:'jim bob',
         email:'jimthebob@mail.com'
@@ -15,8 +24,18 @@ app.get('/user-data', (req, res)=>{
     res.send(user);
 });
 
-app.get('/', (req, res)=>{
-    res.send('<h1>Breh</h1>');
+app.get('/api/get-article', (req, res)=>{
+    const article = {
+        title: 'some awesome article',
+        author: 'john walker',
+        content: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah'
+    };
+
+    res.send(article);
+});
+
+app.get('*', (req, res)=>{
+    res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.listen(PORT, ()=>{
